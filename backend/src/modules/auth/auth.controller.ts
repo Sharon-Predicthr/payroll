1563509d0812d7b54bdfb43048d41d7c9ab4ad37
Sharon-Predicthr <sharon@predicthr.net>
2025-12-02 11:08:38 +0200

    Update auth.controller.ts

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,1 +1,18 @@
+import { Controller, Post, Body, Req } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { Request } from 'express';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login')
+  async login(@Body() body: LoginDto, @Req() req: Request) {
+    return this.authService.login(body, {
+      ip: req.ip,
+      userAgent: req.headers['user-agent'] || '',
+    });
+  }
+}
 
